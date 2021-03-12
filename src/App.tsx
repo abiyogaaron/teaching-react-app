8cce0b851bd5d0494180be0c72c202b63ec79c36
aaron abiyoga <aaron.sulistio@shopee.com>
2021-03-12 22:24:59 +0700

    task this week

--- src/App.tsx
+++ src/App.tsx
@@ -2,87 +2,133 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-interface IData {
-  id: string;
-  name: string;
-  email: string;
-}
-
 interface IAppState {
-  showTable: boolean;
+  fullname: string;
+  age: string;
 }
 
-interface IAppProps {
-  data: IData[]
-}
-
-class App extends React.Component<IAppProps, IAppState> {
-  constructor(props: IAppProps) {
+class App extends React.Component<{}, IAppState> {
+  constructor(props) {
     super(props);
     this.state = {
-      showTable: true,
+      fullname: '',
+      age: '',
     }
   }
 
-  componentDidMount() {
-    alert("did mount App component !")
+  handleBtnClick = () => {
+    alert(`${this.state.fullname} ${this.state.age}`)
   }
 
-  componentWillUnmount() {
-    alert("unmounting App component !")
+  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name } = event.target;
+    const { value } = event.target;
+
+    const newState = { ...this.state };
+    newState[name] = value;
+
+    this.setState(newState);  
   }
 
-  componentDidUpdate() {
-    alert("did update called !");
-  }
-
-  public toggleShowTable = () => {
-    const { showTable } = this.state;
-    this.setState({showTable: !showTable});
-  }
-
-  public renderRow = () => {
-    return this.props.data.map((item) => {
-      return (
-        <tr>
-          <td>{item.id}</td>
-          <td>{item.name}</td>
-          <td>{item.email}</td>
-        </tr>
-      )
-    })
-  }
-
-  public renderTable = () => {
-    if (this.state.showTable) {
-      return (
-        <table className="table">
-          <thead>
-            <tr>
-              <td>id</td>
-              <td>name</td>
-              <td>email</td>
-            </tr>
-          </thead>
-          <tbody>
-            {this.renderRow()}
-          </tbody>
-        </table>
-      )
-    }
-  }
-  
   public render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          {this.renderTable()}
-          <button onClick={this.toggleShowTable}>test</button>
-        </header>
+          <form>
+            <input name="fullname" value={this.state.fullname} placeholder="type fullname here" onChange={this.handleChange} />
+            <br/>
+            <input name="age" value={this.state.age} placeholder="type age here" onChange={this.handleChange} />
+            <br/>
+            <button type="button" onClick={this.handleBtnClick}>SAVE</button>
+          </form>
+       </header>
       </div>
     )
   }
 }
+
+// interface IData {
+//   id: string;
+//   name: string;
+//   email: string;
+// }
+
+// interface IAppState {
+//   showTable: boolean;
+// }
+
+// interface IAppProps {
+//   data: IData[]
+// }
+
+// class App extends React.Component<{}, {}> {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       showTable: true,
+//     }
+//   }
+
+//   componentDidMount() {
+//     alert("did mount App component !")
+//   }
+
+//   componentWillUnmount() {
+//     alert("unmounting App component !")
+//   }
+
+//   componentDidUpdate() {
+//     alert("did update called !");
+//   }
+
+//   public toggleShowTable = () => {
+//     const { showTable } = this.state;
+//     this.setState({showTable: !showTable});
+//   }
+
+//   public renderRow = () => {
+//     return this.props.data.map((item) => {
+//       return (
+//         <tr>
+//           <td>{item.id}</td>
+//           <td>{item.name}</td>
+//           <td>{item.email}</td>
+//         </tr>
+//       )
+//     })
+//   }
+
+//   public renderTable = () => {
+//     if (this.state.showTable) {
+//       return (
+//         <table className="table">
+//           <thead>
+//             <tr>
+//               <td>id</td>
+//               <td>name</td>
+//               <td>email</td>
+//             </tr>
+//           </thead>
+//           <tbody>
+//             {this.renderRow()}
+//           </tbody>
+//         </table>
+//       )
+//     }
+//   }
+  
+//   public render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           {this.renderTable()}
+//           <button onClick={this.toggleShowTable}>test</button>
+//         </header>
+//       </div>
+//     )
+//   }
+// }
 
 export default App;
